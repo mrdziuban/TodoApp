9751687e203102ac9585e456a3a3631ee6e58f77
mrdziuban <mrdziuban@gmail.com>
2013-07-18 08:13:14 -0700

    Add team memberships table/model, users table/model, and associations between teams and users.

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  attr_accessible :name, :team_ids
+
+  has_many :team_memberships
+  has_many :teams, through: :team_memberships
+
+  validates :name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130718143834) do
+ActiveRecord::Schema.define(:version => 20130718151121) do
 
   create_table "items", :force => true do |t|
     t.string   "title"
@@ -34,7 +34,23 @@ ActiveRecord::Schema.define(:version => 20130718143834) do
 
   add_index "projects", ["team_id"], :name => "index_projects_on_team_id"
 
+  create_table "team_memberships", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "team_memberships", ["team_id"], :name => "index_team_memberships_on_team_id"
+  add_index "team_memberships", ["user_id"], :name => "index_team_memberships_on_user_id"
+
   create_table "teams", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
